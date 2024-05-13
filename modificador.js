4c5c6c76012df403209678e286e543f4208d05a5
Moibe <moi.estrello@gmail.com>
2024-05-13 13:07:49 -0600

    Listo para translado

---
+++ modificador.js
@@ -0,0 +1,38 @@
+const element = document.getElementById('barra');
+console.log("Esto es barra...")
+console.log(element.classList)
+
+// element.className = ""
+// console.log("Esto es barra...")
+// console.log(element.classList)
+
+// element.classList.add('bar-20');
+// console.log("Esto es barra...")
+// console.log(element.classList)
+
+// element.classList.remove('oldClass'); // Remove 'oldClass'
+// element.classList.toggle('toggleClass'); // Toggle 'toggleClass' (add if not present, remove if present)
+// console.log(element.classList.contains('someClass')); // Check if 'someClass' exists (true/false)
+
+
+// let string_porcentaje = ""; // Initialize empty string
+
+// for (let i = 1; i <= 100; i++) {
+//   // Create the string_porcentaje
+//   string_porcentaje = `bar-${i}`;
+
+//   // Wrap element class modification in a function
+//   function modifyElementClass() {
+//     element.className = "";
+//     element.classList.add('bar');
+//     element.classList.add(string_porcentaje);
+//   }
+
+//   // Call modifyElementClass with a 3-second delay using setTimeout
+//   setTimeout(modifyElementClass, 5000); // 3000 milliseconds = 3 seconds
+
+//   console.log(string_porcentaje); 
+//}
+
+
+
